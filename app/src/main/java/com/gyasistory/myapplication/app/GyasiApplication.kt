182372ec3005ae8a18c106a586a9e8a799b318bb
Gyasi Story <gyasi.story@cru.org>
2020-05-08 15:53:49 -0400

    Add Flipper to view logic

--- app/src/main/java/com/gyasistory/myapplication/app/GyasiApplication.kt
+++ app/src/main/java/com/gyasistory/myapplication/app/GyasiApplication.kt
@@ -1,11 +1,19 @@
 package com.gyasistory.myapplication.app
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.gyasistory.myapplication.app.service.WordPressService
+
 
 open class GyasiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         WordPressService.instance?.start(applicationContext)
+
+
     }
 }
