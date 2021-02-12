19db8c55b92bcfa70b0fcaaeda102c2dd2f4aa48
Gyasi Story <gyasistory@gmail.com>
2021-02-12 11:32:09 -0500

    code clean up

--- app/src/main/java/com/gyasistory/myapplication/app/GyasiApplication.kt
+++ app/src/main/java/com/gyasistory/myapplication/app/GyasiApplication.kt
@@ -1,13 +1,7 @@
 package com.gyasistory.myapplication.app
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import com.gyasistory.myapplication.app.service.WordPressService
-
 
 open class GyasiApplication : Application() {
     override fun onCreate() {
